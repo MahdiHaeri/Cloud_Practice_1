725043cc21b49e3ea8098df283503a8a09627a03
Mahdi Haeri <mahdihaerim@gmail.com>
2025-10-09 01:19:05 +0330

    Initial commit

---
+++ src/test/kotlin/com/cloudserver/CloudServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cloudserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CloudServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
